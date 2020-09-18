299f65550b54c955fe67bec661f378db75f7d47d
teacher <net94@naver.com>
2020-09-18 17:59:55 +0900

    import 정리

--- src/nonageshop/controller/handler/LoginFormHandler.java
+++ src/nonageshop/controller/handler/LoginFormHandler.java
@@ -5,14 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import nonageshop.controller.Command;
-import nonageshop.dto.Member;
-import nonageshop.service.MemberService;
 
 public class LoginFormHandler implements Command {
-    private MemberService service = new MemberService();
     
     @Override
     public String process(HttpServletRequest request, HttpServletResponse response)
